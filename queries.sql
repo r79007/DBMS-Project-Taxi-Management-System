27feb96e5b5a9409541420b58671be747d8dd086
Raghav Sahni <64552680+Raghav-Sahni@users.noreply.github.com>
2022-02-21 00:25:46 +0530

    re-ordered to prevent errors

--- queries.sql
+++ queries.sql
@@ -1,34 +1,9 @@
-DROP TABLE IF EXISTS `FEEDBACK`;
-
-CREATE TABLE `FEEDBACK` (
-  `Feedback` VARCHAR(30),
-  `User_id` integer,
-  `Bill_no` integer,
-  `employ_id` integer,
-
-  FOREIGN KEY (User_id) REFERENCES USER(user_id),
-  FOREIGN KEY (employ_id) REFERENCES CUSTOMER_SERVICE(employ_id),
-  FOREIGN KEY (Bill_no) REFERENCES BILL_DETAILS(Bill_no)
-);
 DROP TABLE IF EXISTS `CUSTOMER_SERVICE`;
 
 CREATE TABLE `CUSTOMER_SERVICE` (
   `Employ_ID` integer PRIMARY KEY NOT NULL,
   `Name` VARCHAR(30),
   'Contact_No' integer
-);
-DROP TABLE IF EXISTS `TAXI`;
-
-CREATE TABLE `TAXI` (
-  `Taxi_ID` integer PRIMARY KEY NOT NULL,
-  `RegistrationId` integer,
-  `Status` VARCHAR(30),
-  `Model` VARCHAR(30),
-  `Year` integer,
-  `Car_type` VARCHAR(30),
-  `Driver_ID` integer,
-
-  FOREIGN KEY (Driver_ID) REFERENCES DRIVER(Driver_ID)
 );
 DROP TABLE IF EXISTS `USER`;
 
@@ -47,6 +22,45 @@ CREATE TABLE `BILL_DETAILS` (
   `date_bill` integer,
   `tot_amt` integer
 );
+
+DROP TABLE IF EXISTS `DRIVER`;
+
+CREATE TABLE `DRIVER` (
+  `Name` VARCHAR(30),
+  `Gender` VARCHAR(30),
+  `Contact_No` integer,
+  `Age` integer,
+  `Driver_ID` integer PRIMARY KEY NOT NULL , 
+  `Rating` integer
+);
+
+DROP TABLE IF EXISTS `FEEDBACK`;
+
+CREATE TABLE `FEEDBACK` (
+  `Feedback` VARCHAR(30),
+  `User_id` integer,
+  `Bill_no` integer,
+  `employ_id` integer,
+
+  FOREIGN KEY (User_id) REFERENCES USER(user_id),
+  FOREIGN KEY (employ_id) REFERENCES CUSTOMER_SERVICE(employ_id),
+  FOREIGN KEY (Bill_no) REFERENCES BILL_DETAILS(Bill_no)
+);
+
+DROP TABLE IF EXISTS `TAXI`;
+
+CREATE TABLE `TAXI` (
+  `Taxi_ID` integer PRIMARY KEY NOT NULL,
+  `RegistrationId` integer,
+  `Status` VARCHAR(30),
+  `Model` VARCHAR(30),
+  `Year` integer,
+  `Car_type` VARCHAR(30),
+  `Driver_ID` integer,
+
+  FOREIGN KEY (Driver_ID) REFERENCES DRIVER(Driver_ID)
+);
+
 DROP TABLE IF EXISTS `TRIP_DETAILS`;
 
 CREATE TABLE `TRIP_DETAILS` (
@@ -60,14 +74,4 @@ CREATE TABLE `TRIP_DETAILS` (
   `Destination` VARCHAR(30),
 
   FOREIGN KEY (Taxi_ID) REFERENCES TAXI(Taxi_ID)
-);
-DROP TABLE IF EXISTS `DRIVER`;
-
-CREATE TABLE `DRIVER` (
-  `Name` VARCHAR(30),
-  `Gender` VARCHAR(30),
-  `Contact_No` integer,
-  `Age` integer,
-  `Driver_ID` integer PRIMARY KEY NOT NULL , 
-  `Rating` integer
 );
