b59d5467969c9adf1f3aeab80c34f2f95f3317a5
r79007 <rohan20537@iiitd.ac.in>
2022-03-01 00:44:50 +0530

    queries_updated

--- queries_latest.sql
+++ queries_latest.sql
@@ -5,7 +5,8 @@ select Driver_ID from DRIVER where Rating>=4;
 
 SELECT U.UserID from USER U, FEEDBACK F, BILL_DETAILS B where B.Bill_no=F.Bill_no and B.tot_amt>=4000 and F.UserID=U.UserID order by U.UserID asc;
 
-select D.Contact_no from DRIVER D, TAXI T where D.Driver_ID=T.Driver_ID and T.Car_type='SUV';
+select D.Contact_no from DRIVER D, TAXI T, CAR_MANUFACTURER C where D.Driver_ID=T.Driver_ID and T.car_id=C.Car_ID and C.Car_type='SUV';
+
 
 select distinct name,count(age) as cnt from DRIVER group by age  order by cnt DESC;
 
@@ -16,13 +17,8 @@ SELECT * FROM TRIP_DETAILS
 WHERE Pool_num=3
 LIMIT 10;
 
-SELECT COUNT(D.Driver_ID) FROM TRIP_DETAILS TD,DRIVER D,TAXI T WHERE D.driver_ID=T.driver_ID AND T.taxi_ID=TD.taxi_ID AND TD.tripAmount>=2400 AND T.car_type='SUV';
+SELECT COUNT(D.Driver_ID) FROM TRIP_DETAILS TD,DRIVER D,TAXI T, CAR_MANUFACTURER C WHERE D.driver_ID=T.driver_ID AND T.taxi_ID=TD.taxi_ID AND TD.tripAmount>=2400 AND T.car_id=C.Car_ID and C.car_type='SUV';
 
-
-
-
-
-SELECT COUNT(D.Driver_ID) FROM TRIP_DETAILS TD,DRIVER D,TAXI T WHERE D.driver_ID=T.driver_ID AND T.taxi_ID=TD.taxi_ID AND TD.tripAmount>=2400 AND T.car_type='SUV';
 
 ALTER TABLE DRIVER
    MODIFY Age INT NOT NULL CHECK (Age >= 18);
